48658bbafb2d89999e6bb6a38fbc886431e6b0bb
hoseon <chs1025@gmail.com>
2023-03-16 14:32:02 +0900

    Initial Commit

---
+++ pass-wallet-prototype/pass_wallet_prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  pass_wallet_prototypeApp.swift
+//  pass-wallet-prototype
+//
+//  Created by HoSeon Chu on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct pass_wallet_prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
